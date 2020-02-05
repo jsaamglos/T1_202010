b4a72a1f7a556c6427fad0f528482f0972112de7
jsaamglos <60269743+jsaamglos@users.noreply.github.com>
2020-02-05 17:51:58 -0500

    Modificaciones varias.

--- src/model/data_structures/ListaEncadenada.java
+++ src/model/data_structures/ListaEncadenada.java
@@ -1,109 +1,94 @@
 package model.data_structures;
 
-public class ListaEncadenada <T>
-{
-    //apuntador a primer elemento
-    private Node<T> primerElemento;
+public class ListaEncadenada<T> implements IListaEncadenada<T> {
+	// apuntador a primer elemento
+	private Node<T> primerElemento;
 
-    //apuntador a ultimo elemento
-    private Node<T> ultimoElemento;
+	// apuntador a ultimo elemento
+	private Node<T> ultimoElemento;
 
-    //tamaño de la lista    
-    private int tamaño;
+	// tamano de la lista
+	private int tamano;
 
-    //constructor
-    public ListaEncadenada()
-    {
-        tamaño = 0;
-    }
+	// constructor
+	public ListaEncadenada() {
+		tamano = 0;
+	}
 
-    //agrega un elemento
-    public void agregarElemento(T dato)
-    {
-        
-        if(primerElemento == null)
-        {
-            Node<T> nodo = new Node<T>(dato, null);
-            //agrega el nodo como ultimo y primer elemento
-            primerElemento = nodo;
-            ultimoElemento = nodo;
-        }else
-        {
-            //pone el elemento al final de la fila
-            ultimoElemento = new Node<T>(dato, ultimoElemento);
-        }
+	// agrega un elemento
+	public void agregarElemento(T dato) {
 
-        //incrementa el tamaño
-        tamaño++;
-    }
+		if (primerElemento == null) {
+			Node<T> nodo = new Node<T>(dato, null);
+			// agrega el nodo como ultimo y primer elemento
+			primerElemento = nodo;
+			ultimoElemento = nodo;
+		} else {
+			// pone el elemento al final de la fila
+			ultimoElemento = new Node<T>(dato, ultimoElemento);
+		}
 
-    //retorna el tamaño de la lista 
-    public int darTamaño()
-    {
-        return tamaño;
-    }
+		// incrementa el tamano
+		tamano++;
+	}
 
-    //retorna el elemento dado por parametro
-    public boolean existeElemento(T dato)
-    {
-        //crea nodo igual al primer elemento
-        Node<T> nodo = primerElemento;
+	// retorna el tamano de la lista
+	public int darTamano() {
+		return tamano;
+	}
 
-        //itera los elementos hasta que se acaben o encuentre el elemento
-        while(nodo != null && !nodo.getElemento().equals(dato))
-        {
-            nodo = nodo.getSiguiente();
-        }
+	// retorna el elemento dado por parametro
+	public boolean existeElemento(T dato) {
+		// crea nodo igual al primer elemento
+		Node<T> nodo = primerElemento;
 
-        //retorna true si encontro el elemento
-        return nodo != null;
-    }
+		// itera los elementos hasta que se acaben o encuentre el elemento
+		while (nodo != null && !nodo.getElemento().equals(dato)) {
+			nodo = nodo.getSiguiente();
+		}
 
-    public void eliminarElemento(T dato)
-    {
-        //crea nodo igual al primer elemento
-        Node<T> nodo = primerElemento;
+		// retorna true si encontro el elemento
+		return nodo != null;
+	}
 
-        //itera los elementos hasta que se acaben o encuentre el elemento
-        while(nodo != null && !nodo.getElemento().equals(dato))
-        {
-            nodo = nodo.getSiguiente();
-        }
+	public void eliminarElemento(T dato) {
+		// crea nodo igual al primer elemento
+		Node<T> nodo = primerElemento;
 
-        //se encontro el nodo?
-        if(nodo != null)
-        {
-            //borra el nodo de la existencia
-            nodo.getAnterior().setSiguiente(nodo.getSiguiente());
-            nodo.getSiguiente().setAnterior(nodo.getAnterior());
+		// itera los elementos hasta que se acaben o encuentre el elemento
+		while (nodo != null && !nodo.getElemento().equals(dato)) {
+			nodo = nodo.getSiguiente();
+		}
 
-            //baja en uno el tamaño
-            tamaño--;
+		// se encontro el nodo?
+		if (nodo != null) {
+			// borra el nodo de la existencia
+			nodo.getAnterior().setSiguiente(nodo.getSiguiente());
+			nodo.getSiguiente().setAnterior(nodo.getAnterior());
 
-        }else
-        {
-            //Bruh, no esta el nodo
-            System.out.println("No se encontro el elemento a eliminar.");
-        }
+			// baja en uno el tamano
+			tamano--;
 
-    }
+		} else {
+			// Bruh, no esta el nodo
+			System.out.println("No se encontro el elemento a eliminar.");
+		}
 
-    //existen elementos en la lista?
-    public boolean isEmpty()
-    {
-        return tamaño == 0;
-    }
+	}
 
-    //retorna primer elemento
-    public T darPrimerElemento()
-    {
-        return primerElemento.getElemento();
-    }
+	// existen elementos en la lista?
+	public boolean isEmpty() {
+		return tamano == 0;
+	}
 
-    //retorna ultimo elemento
-    public T darUltimoElemento()
-    {
-        return ultimoElemento.getElemento();
-    }
+	// retorna primer elemento
+	public T darPrimerElemento() {
+		return primerElemento.getElemento();
+	}
+
+	// retorna ultimo elemento
+	public T darUltimoElemento() {
+		return ultimoElemento.getElemento();
+	}
 
 }
